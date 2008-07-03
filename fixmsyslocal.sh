5447040a148ca1cc4369471b6b0bf1ffbf0b8576
Lars Christensen <larsch@belunktum.dk>
2008-07-03 21:29:03 +0200

    improved handling of funny msys mounting by moving all usr/* to /

--- fixmsyslocal.sh
+++ fixmsyslocal.sh
@@ -3,10 +3,8 @@
 # the MSYS 1.0.11 Technology preview. It has /usr mounted to the same
 # path as /. Hence /usr/local is unaccessible.
 cd /
-if [ ! -e usr/local ]; then
-        echo "moving"
-	/bin/mv $1/usr/local $1/local
+if [ -e $1/usr ]; then
+    echo "moving"
+    cp -a $1/usr/* .
+    rm -rf $1/usr
 fi
-#if [ -e usr/bin/m4.exe ]; then
-#    mv usr/bin/m4.exe bin/m4.exe
-#fi
